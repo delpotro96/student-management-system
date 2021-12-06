39f4dd9ad1070fd3ec9f603b9c6f3bb07d3ce5dc
delpotro96 <freeone1224@gmail.com>
2021-12-06 17:23:06 +0900

    Update Report.java

--- src/main/java/com/kyunghunlee/studentmanagementsystem/entity/Report.java
+++ src/main/java/com/kyunghunlee/studentmanagementsystem/entity/Report.java
@@ -16,28 +16,20 @@ public class Report {
 
   @Id @GeneratedValue private Long id;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer korean_1;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer korean_2;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer english_1;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer english_2;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer math_1;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer math_2;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer history_1;
 
-  @ColumnDefault("점수를 입력해주세요")
   private Integer history_2;
 
   @ManyToOne private Student student;
